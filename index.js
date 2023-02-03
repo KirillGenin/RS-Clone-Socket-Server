81a97ffc3fb77896d9457b6a031d45634930e7d6
KirillGenin <106728834+KirillGenin@users.noreply.github.com>
2023-02-03 20:00:44 +0300

    Update index.js

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ app.get('/', (req, res) => {
   res.send('Приложение запущено');
 });
 
-const PORT = process.env.PORT ?? 5000;
+const PORT = process.env.PORT ?? 3000;
 
 server.listen(PORT, () => {
   console.log(`listening on PORT: ${PORT}`);
