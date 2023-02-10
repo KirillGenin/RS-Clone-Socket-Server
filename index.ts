39a758a473d529607ff215b8f03aef33be42f402
KirillGenin <106728834+KirillGenin@users.noreply.github.com>
2023-02-10 19:03:18 +0300

    Update index.ts

--- index.ts
+++ index.ts
@@ -1,19 +1,21 @@
-const express = require('express');
-const http = require('http');
-import { Server, Socket } from 'socket.io';
-const cors = require('cors');
+import express from 'express';
+import http from 'http';
+import { Server, Socket } from "socket.io";
+import cors from 'cors';
 
 const app = express();
 const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: 'https://chat-socket-test.netlify.app/',
+    origin: '*',
     methods: ['GET', 'POST'],
   },
 });
 
-app.use(cors({ origin: '*' })); // CORS
+app.use(cors({
+  origin: '*'
+})); // CORS
 
 app.get('/', (req, res) => {
   res.send('Приложение запущено');
