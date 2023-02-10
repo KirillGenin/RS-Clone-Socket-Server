ae25d459c8ebef7ee6746cd69742aba83e59faa6
KirillGenin <106728834+KirillGenin@users.noreply.github.com>
2023-02-10 18:51:53 +0300

    Update index.ts

--- index.ts
+++ index.ts
@@ -1,7 +1,7 @@
-import express from 'express';
-import http from 'http';
+const express = require('express');
+const http = require('http');
 import { Server, Socket } from 'socket.io';
-import cors from 'cors';
+const cors = require('cors');
 
 const app = express();
 const server = http.createServer(app);
